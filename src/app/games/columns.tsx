f3f1408fa45260d90581e4b3c23c6e1778e151a4
Xun Ruan <xunruan@icloud.com>
2024-06-29 21:37:16 +0800

    add row actions

--- src/app/games/columns.tsx
+++ src/app/games/columns.tsx
@@ -1,6 +1,16 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 export type Game = {
   slug: string;
@@ -37,6 +47,36 @@ export const columns: ColumnDef<Game>[] = [
         currency: "USD",
       }).format(amount);
       return <div className={"text-end font-medium"}>{formatted}</div>;
+    },
+  },
+
+  // row actions
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const game = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant={"ghost"} className={"size-8 p-0"}>
+              <span className={"sr-only"}>Open menu</span>
+              <MoreHorizontal className={"h-4 w-4"} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align={"end"}>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(game.slug)}
+            >
+              Copy game slug
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
